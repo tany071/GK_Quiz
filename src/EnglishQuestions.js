aff40b99564018b014aae7b787238a28bb93c14d
tany071 <tanyasinha7u@gmail.com>
2023-05-19 10:53:28 +0530

    initial commit

---
+++ src/EnglishQuestions.js
@@ -0,0 +1,50 @@
+export const englishData = [
+  {
+    question: 'What is the capital of Cyprus?',
+    Options: ['Nicosia', 'Peyia', 'Kyrenia', 'Limassol'],
+    correct: 1,
+    marked: -1,
+  },
+  {
+    question: 'What is the capital of Sudan?',
+    Options: ['Khartoum', 'Gitega', 'Tripoli', 'Berlin'],
+    correct: 2,
+    marked: -1,
+  },
+  {
+    question: 'What is the national currrency of Maldives?',
+    Options: ['Regel', 'Rufiyaa', 'Som', 'Manat'],
+    correct: 1,
+    marked: -1,
+  },
+  {
+    question: 'What is the national currency of South Africa?',
+    Options: ['Rand', 'Pound', 'Franc', 'Dinar'],
+    correct: 3,
+    marked: -1,
+  },
+  {
+    question: 'What is the national currency of Poland?',
+    Options: ['Yen', 'Zloty', 'Dollar', 'Peso'],
+    correct: 2,
+    marked: -1,
+  },
+  {
+    question: 'What is the national currency of Saudi Arabia?',
+    Options: ['Riyal', 'Shekel', 'Rupiah', 'Dinar'],
+    correct: 4,
+    marked: -1,
+  },
+  {
+    question: 'What is the national currency of Cambodia?',
+    Options: ['Peso', 'Rupee', 'Riel', 'Taka'],
+    correct: 4,
+    marked: -1,
+  },
+  {
+    question: 'The national currency of the United States of America (USA) is ',
+    Options: ['Euro', 'Dollar', 'Rand', 'Yen'],
+    correct: 1,
+    marked: -1,
+  },
+];
